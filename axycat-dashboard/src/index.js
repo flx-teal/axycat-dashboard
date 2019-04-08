d48668a439113af291264d7fac0b88a40d8d12cc
CKMoJKE <rogivnazariy@gmail.com>
2019-04-08 22:00:30 +0300

    Added react-router-dom

--- axycat-dashboard/src/index.js
+++ axycat-dashboard/src/index.js
@@ -1,12 +1,25 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Home from './components/Home';
+import Details from './components/Details';
+import NewProjectPopUp from './components/NewProjectPopUp';
+import Listing from './components/Listing';
+import Error from './components/Error';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello Epam</h1>
-            </div>
+            <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Home} exact/>
+                <Route path="/detail" component={Details}/>
+                <Route path="/listing" component={Listing}/>
+                <Route path="/newproject" component={NewProjectPopUp}/>
+                <Route component={Error}/>
+            </Switch>
+            </BrowserRouter>
         );
     }
 }
