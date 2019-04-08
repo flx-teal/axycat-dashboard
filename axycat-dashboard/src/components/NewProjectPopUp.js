ed85ba2022d04f3a425e7a2a9edb5281a3b331d8
CKMoJKE <rogivnazariy@gmail.com>
2019-04-08 21:55:15 +0300

    Initial commit

---
+++ axycat-dashboard/src/components/NewProjectPopUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewProjectPopUp = () => {
+    return (
+        <div>
+            <h1>NewProjectPopUp</h1>
+        </div>
+    )
+};
+
+export default NewProjectPopUp
