738bf72e26d1700f17dc39de9f62535153f64515
Ruslan Rykhtyk <ruslan.rykhtyk@gmail.com>
2019-04-19 18:39:38 +0300

    added : - Inner-list-item component and style; -optimized title component

--- axycat-dashboard/src/components/details-components/TitleComponent.js
+++ axycat-dashboard/src/components/details-components/TitleComponent.js
@@ -2,26 +2,12 @@ import React from 'react';
 import './TitleComponent.scss';
 
 function TitleComponent(props) {
-  if (props.title && props.subtitle) {
-    return (
-      <div className='title-container'>
-        <h2 className='title'>{props.title}</h2>
-        <h3 className='sub-title'>{props.subtitle}</h3>
-      </div>
-    );
-  } else if (props.title && props.subtitle === undefined) {
-    return (
-      <div className='title-container'>
-        <h2 className='title'>{props.title}</h2>
-      </div>
-    );
-  } else if (props.title === undefined && props.subtitle) {
-    return (
-      <div className='title-container'>
-        <h3 className='sub-title'>{props.subtitle}</h3>
-      </div>
-    );
-  }
+  return (
+    <div className='title-container'>
+      {props.title && <h2 className='title'>{props.title}</h2>}
+      {props.subtitle && <h3 className='sub-title'>{props.subtitle}</h3>}
+    </div>
+  );
 }
 
 export default TitleComponent;
