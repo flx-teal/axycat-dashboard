37f2b0bc7e396f760179d972213aa4fb9b442d36
CKMoJKE <rogivnazariy@gmail.com>
2019-05-01 12:24:04 +0300

    Add functions to use Firebase with examples

--- axycat-dashboard/src/config/fbConfig.js
+++ axycat-dashboard/src/config/fbConfig.js
@@ -3,7 +3,7 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 // Initialize Firebase
-var config = {
+const config = {
     apiKey: "AIzaSyDj75w9OsPBE4wGa7p5iqkclsNZIOKK21c",
     authDomain: "flx-teal.firebaseapp.com",
     databaseURL: "https://flx-teal.firebaseio.com",
@@ -13,6 +13,57 @@ var config = {
 };
 
 firebase.initializeApp(config);
-firebase.firestore();//.settings({timestampsInSnapshots: false});
+const db = firebase.firestore();
 
-export default firebase;
+export const addErrorToCloud = (data) => {
+    firebase.firestore().collection("errors").add({
+        ...data
+    })
+        .then(function (docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+            console.error("Error adding document: ", error);
+        });
+};
+
+//Recive all Reports from Firebase
+export const getAllReportsFromCloud = (callBack) => {
+    db.collection("errors").get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+            callBack(doc.data())
+        });
+    });
+};
+
+//Example All Reports
+/*
+function showData(data){
+    console.log(data.violations);
+}
+
+getAllReportsFromCloud(showData);
+*/
+
+//Recive Report from Firebase by ID
+export const getReportFromCloudById = (id, callback) => {
+    let docRef = db.collection("errors").doc(id);
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            callback(doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.error("Error getting document:", error);
+    });
+};
+
+//Example By ID
+/*
+function showData(data){
+    console.log(data);
+}
+
+getReportFromCloudById('0ujvlxp97Jz1lSjNEnwY', showData);*/
