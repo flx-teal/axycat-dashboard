e70dec2872f97ee96fd16d65c182f9443c3adba7
CKMoJKE <rogivnazariy@gmail.com>
2019-05-03 09:52:46 +0300

    add async await to getAllReportsFromCloud
    need to test

--- axycat-dashboard/src/config/fbConfig.js
+++ axycat-dashboard/src/config/fbConfig.js
@@ -16,55 +16,45 @@ firebase.initializeApp(config);
 const db = firebase.firestore();
 
 export const addErrorToCloud = async (checkResult, projectData = {}) => {
-  try {
-    const docRef = await firebase.firestore().collection("errors").add({
-      ...checkResult,
-      projectData
-    });
-    console.log("Document written with ID: ", docRef.id);
-    return docRef.id;
-  } catch (error) {
-    console.error("Error adding document: ", error);
-  }
+    try {
+        const docRef = await firebase.firestore().collection("errors").add({
+            ...checkResult,
+            projectData
+        });
+        console.log("Document written with ID: ", docRef.id);
+        return docRef.id;
+    } catch (error) {
+        console.error("Error adding document: ", error);
+    }
 };
 
 //Receive all Reports from Firebase
-export const getAllReportsFromCloud = (callBack) => {
-    db.collection("errors").get().then((snapshot) => {
-        snapshot.docs.forEach(doc => {
-            callBack(doc.data())
-        });
-    });
+export const getAllReportsFromCloud = async () => {
+    try {
+        const response = await db.collection("errors").get();
+        const data = await response.docs;
+        await data.map(report => {
+            return report.data();
+        })
+    }
+    catch
+        (error) {
+        console.error("Error getting document:", error);
+    }
+    return {}
 };
-
-//Example All Reports
-/*
-function showData(data){
-    console.log(data.violations);
-}
-
-getAllReportsFromCloud(showData);
-*/
 
 //Receive Report from Firebase by ID
 export const getReportFromCloudById = async id => {
-  try {
-    const doc = await db.collection("errors").doc(id).get();
-    if (doc.exists) {
-      return doc.data();
+    try {
+        const doc = await db.collection("errors").doc(id).get();
+        if (doc.exists) {
+            return doc.data();
+        }
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    } catch (error) {
+        console.error("Error getting document:", error);
     }
-    // doc.data() will be undefined in this case
-    console.log("No such document!");
-  } catch (error) {
-    console.error("Error getting document:", error);
-  }
-  return {}
+    return {}
 };
-
-//Example By ID
-/*
-function showData(data){
-    console.log(data);
-}
-
-getReportFromCloudById('0ujvlxp97Jz1lSjNEnwY', showData);*/
