4c40e1acdbba6c508a03390c3c7649da6d27556f
CKMoJKE <rogivnazariy@gmail.com>
2019-05-09 15:26:33 +0300

    add Authentication component, replace buttons from index

---
+++ axycat-dashboard/src/components/Authentication/Authentication.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SignUp from "./SignUp/SignUp";
+import Login from "./Login/Login";
+
+class Authentication extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isSignUp: false,
+            isLogin: false
+        };
+    }
+
+    handleSignUp = () => {
+        this.setState(prevState => ({
+            isSignUp: !prevState.isSignUp
+        }));
+    };
+
+    handleLogin = () => {
+        this.setState(prevState => ({
+            isLogin: !prevState.isLogin
+        }));
+    };
+
+    render () {
+        return(
+            <div className='buttons-container'>
+                <button onClick={this.handleSignUp} className='btn btn-blue' name='Sign up'>Sing Up</button>
+                <div>{this.state.isSignUp && <SignUp handleSignUp={this.handleSignUp}/>}</div>
+                <button onClick={this.handleLogin} className='btn btn-white' name='Sign up'>Login</button>
+                <div>{this.state.isLogin && <Login handleLogin={this.handleLogin}/>}</div>
+            </div>
+        )
+    }
+}
+
+export default Authentication
--- axycat-dashboard/src/index.js
+++ axycat-dashboard/src/index.js
@@ -8,30 +8,9 @@ import Error from './components/Error';
 import TitleComponent from './components/details-components/TitleComponent'
 import './index.scss'
 import ProjectItemNavigation from "./components/ProjectItemNavigation/ProjectItemNavigation";
-import SignUp from "./components/SignUp/SignUp";
-import Login from "./components/Login/Login";
+import Authentication from "./components/Authentication/Authentication";
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isSignUp: false,
-            isLogin: false
-        };
-    }
-
-    handleSignUp = () => {
-        this.setState(prevState => ({
-            isSignUp: !prevState.isSignUp
-        }));
-    };
-
-    handleLogin = () => {
-        this.setState(prevState => ({
-            isLogin: !prevState.isLogin
-        }));
-    };
-
     render() {
         return (
             <BrowserRouter>
@@ -57,15 +36,7 @@ class App extends Component {
                                 Listing
                             </NavLink>
                         </nav>
-                        <div className='buttons-container'>
-
-                            <button onClick={this.handleSignUp} className='btn btn-blue' name='Sign up'>Sing Up</button>
-                            <div>{this.state.isSignUp && <SignUp handleSignUp={this.handleSignUp}/>}</div>
-
-                            <button onClick={this.handleLogin} className='btn btn-white' name='Sign up'>Login</button>
-                            <div>{this.state.isLogin && <Login handleLogin={this.handleLogin}/>}</div>
-
-                        </div>
+                        <Authentication />
                     </header>
                     <Switch>
                         <Route path="/" component={Home} exact/>
