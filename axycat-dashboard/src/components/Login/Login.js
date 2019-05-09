2cd2d6c1847cb5f5136cf2433736c06b56b02b95
CKMoJKE <rogivnazariy@gmail.com>
2019-05-09 15:21:18 +0300

    add login form

--- axycat-dashboard/src/components/Login/Login.js
+++ axycat-dashboard/src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Login.scss'
 
 class Login extends React.Component {
     constructor(props) {
@@ -8,10 +9,23 @@ class Login extends React.Component {
     render() {
         return (
             <div>
-                {!this.props.isLogin && <div className='issues-popup'>
-                    <div className='issues-popup-inner'>
-                        <div className='issues-popup-inner-wrapper'>
-                            <h1 onClick={this.props.handleLogin}>Login</h1>
+                {!this.props.isLogin && <div className='login-popup'>
+                    <div className='login-popup-inner'>
+                        <div className='login-popup-inner-wrapper'>
+                            <span onClick={this.props.handleLogin} className='login-close'></span>
+                            <div className='login-info'>
+                                <h2>SignUp</h2>
+                                <form className="login-info-form">
+                                    <input type="text" placeholder='Full Name *'/>
+                                    <input type="text" placeholder='Your Email *'/>
+
+                                    <div className='login-info-form-check'>
+                                        <input type="checkbox"/>
+                                        <span>Remember password?</span>
+                                    </div>
+                                </form>
+                                <button className='btn btn-blue'>Sign Up</button>
+                            </div>
                         </div>
                     </div>
                 </div>}
